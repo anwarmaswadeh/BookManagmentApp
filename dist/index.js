9b4cac35d6636e438dbfe32a0e86cf639fcd6bf9
anwarmaswadeh <anwarmaswadeh77@gmail.com>
2023-08-21 12:13:48 +0300

    add systemd.service

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,14 @@ import express from 'express';
 import bookRouter from "./routers/book.js";
 const app = express();
 const PORT = 80;
-
 app.use(express.json());
-
 app.get('/', (req, res) => {
     res.send('Server Up!');
 });
-
-app.get('/health', (req, res) => {
+app.get("/health", function (req, res) {
     res.sendStatus(200);
 });
-
 app.use("/books", bookRouter);
-
 app.listen(PORT, () => {
     console.log(`App is running and Listening on port ${PORT}`);
 });
